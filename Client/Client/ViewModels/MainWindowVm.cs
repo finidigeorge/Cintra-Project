6059e0de12846331347a63f4c6d5b6c2cd987d3f
Alex Kataev <akataev@ibscapital.com.au>
2017-12-16 19:34:34 +1100

    Fody configuration fix + Main window refactoring

--- Client/Client/ViewModels/MainWindowVm.cs
+++ Client/Client/ViewModels/MainWindowVm.cs
@@ -15,66 +15,20 @@ namespace Client.ViewModels
 {
     public class MainWindowVm: BaseVm
     {
-        public ICommand ShowBookingWindowCommand { get; }
-        public ICommand ShowLoginDialogCommand { get; }
-        public ICommand ShowChangePasswordDialogCommand { get; }
-        public ICommand ShowExitDialogCommand { get; }
-        public ICommand RunClientHistoryReportCommand { get; }
+        public ICommand ShowBookingWindowCommand { get; set; }
+        public ICommand ShowLoginDialogCommand { get; set; }
+        public ICommand ShowChangePasswordDialogCommand { get; set; }
+        public ICommand ShowExitDialogCommand { get; set; }
+        public ICommand RunClientHistoryReportCommand { get; set; }
 
         public AuthVm AuthVm { get; }
-        public Dictionary<string, IEditableSelectableReference<object>> TabsDictionary = new Dictionary<string, IEditableSelectableReference<object>>();
-
-        bool bookingWindowIsRunning = false;
+        public Dictionary<string, IEditableSelectableReference<object>> TabsDictionary = new Dictionary<string, IEditableSelectableReference<object>>();        
 
         public MainWindowVm()
-        {
-            ShowBookingWindowCommand = new Command<object>(() =>
-            {                
-                var thread = new Thread(() =>
-                {
-                    var bookingWindow = new BookingWindow();
-                    bookingWindowIsRunning = true;                    
-                    bookingWindow.Show();
-
-                    bookingWindow.Closed += (sender2, e2) => { bookingWindow.Dispatcher.InvokeShutdown(); bookingWindowIsRunning = false; };
-
-                    System.Windows.Threading.Dispatcher.Run();
-                });
-
-                thread.SetApartmentState(ApartmentState.STA);
-                thread.Start();                                                
-
-            }, x => AuthVm.IsAuthenticated && !bookingWindowIsRunning);
-
-            ShowLoginDialogCommand = new Command<object>(() =>
-            {
-                new LoginWindow() {DataContext = AuthVm}.ShowDialog(); 
-                
-            }, x => true);
-
-            ShowChangePasswordDialogCommand = new Command<object>(() =>
-                {
-                    new ChangePasswordWindow() {DataContext = new ChangePasswordVm()}.ShowDialog();
-                }, x => true);
-
-            ShowExitDialogCommand = new Command<object>(ExitAppCommandAction, x => true);
-
-            RunClientHistoryReportCommand = new Command<object>(() =>
-            {
-                new ClientLessonsReportWindow().ShowDialog();
-            }, x => AuthVm.IsAuthenticated && AuthVm.IsAdmin);
-
-
+        {            
             AuthVm = new AuthVm(new AuthClient(), new UserRolesClient());
         }              
 
-        private void ExitAppCommandAction()
-        {
-            MessageBoxResult result = CustomMessageBox.Show(Messages.EXIT_APP_MSG, "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
-            if (result == MessageBoxResult.Yes)
-            {
-                Application.Current.Shutdown();
-            }
-        }
+        
     }
 }
--- Client/Client/Windows/MainWindow.xaml.cs
+++ Client/Client/Windows/MainWindow.xaml.cs
@@ -1,23 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Client.ViewModels;
 using Common;
-using RestApi;
-using RestClient;
 using WPFCustomMessageBox;
 using Client.Commands;
+using System.Threading;
+using Client.Windows;
+using Client.Windows.Reports;
 
 namespace Client
 {
@@ -28,9 +19,48 @@ namespace Client
     {
         public MainWindowVm Model => (MainWindowVm)Resources["ViewModel"];
 
+        bool bookingWindowIsRunning = false;
+        private string selectedTab = "Horses";
+
         public MainWindow()
         {
-            InitializeComponent();            
+            InitializeComponent();
+
+            Model.ShowBookingWindowCommand = new Command<object>(() =>
+            {
+                var thread = new Thread(() =>
+                {
+                    var bookingWindow = new BookingWindow() { Owner = this };
+                    bookingWindowIsRunning = true;
+                    bookingWindow.Show();
+
+                    bookingWindow.Closed += (sender2, e2) => { bookingWindow.Dispatcher.InvokeShutdown(); bookingWindowIsRunning = false; };
+
+                    System.Windows.Threading.Dispatcher.Run();
+                });
+
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start();
+
+            }, x => Model.AuthVm.IsAuthenticated && !bookingWindowIsRunning);
+
+            Model.ShowLoginDialogCommand = new Command<object>(() =>
+            {
+                new LoginWindow() { DataContext = Model.AuthVm, Owner = this }.ShowDialog();
+
+            }, x => true);
+
+            Model.ShowChangePasswordDialogCommand = new Command<object>(() =>
+            {
+                new ChangePasswordWindow() { DataContext = new ChangePasswordVm(), Owner = this }.ShowDialog();
+            }, x => true);
+
+            Model.ShowExitDialogCommand = new Command<object>(ExitAppCommandAction, x => true);
+
+            Model.RunClientHistoryReportCommand = new Command<object>(() =>
+            {
+                new ClientLessonsReportWindow() { Owner = this }.ShowDialog();
+            }, x => Model.AuthVm.IsAuthenticated && Model.AuthVm.IsAdmin);
         }
 
         private bool CheckAndWarnAuth()
@@ -43,7 +73,14 @@ namespace Client
             return result;
         }
 
-        private string selectedTab = "Horses";
+        private void ExitAppCommandAction()
+        {
+            MessageBoxResult result = CustomMessageBox.Show(Messages.EXIT_APP_MSG, "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }        
 
         private async void OnTabSelectionChanged(Object sender, SelectionChangedEventArgs args)
         {
@@ -53,9 +90,9 @@ namespace Client
                     { "Clients", ClientsRefView.Model.RefreshDataCommand },                    
                     { "Horses", HorsesRefView.Model.RefreshDataCommand },
                     { "PaymentTypes", PaymentTypesRefView.Model.RefreshDataCommand },
-                    { "Services", ServicesRefView.Model.RefreshDataCommand },                    
-                    { "UserRoles", UserRolesRefView.Model.RefreshDataCommand },
+                    { "Services", ServicesRefView.Model.RefreshDataCommand },                                        
                     { "Users", UsersRefView.Model.RefreshDataCommand },
+                    //{ "UserRoles", UserRolesRefView.Model.RefreshDataCommand },
                 };
 
             if (sender is TabControl tc)
