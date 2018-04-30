3305d0e658aef5162d496e35f641d86223e280d4
Alex Kataev <akataev@ibscapital.com.au>
2018-04-30 20:55:52 +1000

    change default week number for fortnight booking

--- DbUpdater/DbUpdater/Scripts/022_booking_template_mdt_forthnight.sql
+++ DbUpdater/DbUpdater/Scripts/022_booking_template_mdt_forthnight.sql
@@ -1,2 +1,2 @@
 alter table bookings_template_metadata add column is_fortnightly BOOLEAN default 0 not null;
-alter table booking_templates add column is_first_week BOOLEAN default 0 not null;
+alter table booking_templates add column is_first_week BOOLEAN default 1 not null;
