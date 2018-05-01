35d23fabd109dc60ae3b4a9774fbaa5a315e9827
Alex Kataev <alex.kataev@avocado.com.au>
2018-05-01 15:30:05 +1000

    Add typeahead search in booking window

---
+++ Client/Client/Controls/TypeAheadComboBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Client.Controls
+{
+    public class TypeAheadComboBox: ComboBox
+    {
+        public TypeAheadComboBox()
+        {
+            IsEditable = true;
+            StaysOpenOnEdit = true;
+            PreviewTextInput += new TextCompositionEventHandler(OnPreviewTextInputHandler);
+        }
+
+        protected void OnPreviewTextInputHandler(object sender, TextCompositionEventArgs e)
+        {
+            IsDropDownOpen = true;
+        }
+    }
+}
