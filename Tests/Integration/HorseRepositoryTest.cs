a807bfa7d63e1b7ce313d196ce1dfb617c0c0a20
Alex Kataev <akataev@ibscapital.com.au>
2018-05-01 20:02:50 +1000

    Integration tests WiP

---
+++ Tests/Integration/HorseRepositoryTest.cs
@@ -0,0 +1,25 @@
+﻿using DataModels;
+using NUnit.Framework;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class HorseRepositoryTest
+    {
+        readonly HorsesRepository _respository = new HorsesRepository();
+
+        [Test]
+        public async Task HorseRepositoryCreate()
+        {
+            var horse = new Hors() { Nickname = "TestHorse", MaxWorkingHours = 4 };
+
+            var id = await _respository.Create(horse);
+            Assert.AreNotEqual(0, id);
+        }
+    }
+}
---
+++ Tests/Integration/IntegrationTestBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.Integration
+{
+    public abstract class IntegrationTestBase
+    {
+        [SetUp]
+        public void IntegrationTestBaseSetUp()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionStringsAppSettings = new ConnectionStringsAppSettings();
+            configuration.GetSection("ConnectionStrings").Bind(connectionStringsAppSettings);            
+        }
+
+    }
+}
